cde16be31a08802eee858927eb281a3a751914ac
valexq <valexq11@gmail.com>
2023-05-21 15:03:32 -0500

    Creating Tickets class

---
+++ ParcialVanessaAlfaro/DAL/Entities/Tickets.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ParcialVanessaAlfaro.DAL.Entities
+{
+    public class Tickets
+    {
+        [Required]
+        [Key]
+        [Display(Name = "Código de boleta")]
+        public Guid Id { get; set; }
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        [Display(Name = "Uso")]
+        public Boolean IsUsed { get; set; }
+        [Display(Name = "Portería de entrada")]
+        public string? EntranceGate { get; set; }
+
+    }
+}
