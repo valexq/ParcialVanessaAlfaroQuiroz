93e8cfe159e87cef8f12fe96e8b5e376eb775765
valexq <valexq11@gmail.com>
2023-05-21 16:02:22 -0500

    Creation of DataBase and setting configurations

---
+++ ParcialVanessaAlfaro/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ParcialVanessaAlfaro.DAL.Entities;
+
+namespace ParcialVanessaAlfaro.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Tickets> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Tickets>().HasIndex(c=>c.Id).IsUnique();
+        }
+    }
+}
--- ParcialVanessaAlfaro/Program.cs
+++ ParcialVanessaAlfaro/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ParcialVanessaAlfaro.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataBaseContext>(o =>
+{
+o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
