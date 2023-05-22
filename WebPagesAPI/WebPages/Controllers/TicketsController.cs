c93403b9ea6cd63e9c03235a07b076e6110becf8
valexq <valexq11@gmail.com>
2023-05-22 10:01:55 -0500

    Creating WebPages and controllers

---
+++ WebPagesAPI/WebPages/Controllers/TicketsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ParcialVanessaAlfaro.DAL.Entities;
+using System.Text.Json.Serialization;
+
+namespace WebPages.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public TicketsController(IHttpClientFactory httpClient )
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7030/api/Tickets/GetTicketsInformation";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Ticket> tickets = JsonConvert.DeserializeObject<List<Ticket>>(json);
+            return View(tickets);
+        }
+    }
+}
